bd66d8cecea03c05233671de3d5032d2f0f2829c
danyoh <danyoh@gmail.com>
2025-07-18 22:09:21 +0900

    1

---
+++ src/main/java/com/example/gspr01/Gspr01Application.java
@@ -0,0 +1,13 @@
+package com.example.gspr01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gspr01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gspr01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gspr01/Gspr01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gspr01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gspr01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
